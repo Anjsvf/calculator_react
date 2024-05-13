860693f5100a1b3db50e12822f4dbd0ef4c74b3e
antonio santos <antoniosantos.devweb@gmail.com>
2024-05-13 13:29:01 -0300

    React calculator add

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Calculator  from './main/Calculator'
+import './index.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+  <div>
+    <h2>calculadora</h2>
+  <Calculator />
+  </div>
+   
+  </React.StrictMode>,
+)
